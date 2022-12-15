1bb2eb597712395a8044b3c91b33bb4a3090e2ff
aedemir789 <aedemir789@gmail.com>
2022-12-15 15:36:29 +0300

    add

--- app_service/app_service.tf
+++ app_service/app_service.tf
@@ -13,8 +13,8 @@ resource "azurerm_service_plan" "tf_app_service_plan" {
   location            = azurerm_resource_group.tf_resource_group.location
   resource_group_name = azurerm_resource_group.tf_resource_group.name
 
-  os_type             = "Windows"
-  sku_name            = "F1"
+  os_type  = "Windows"
+  sku_name = "F1"
 }
 
 resource "azurerm_windows_web_app" "tf_web_app" {
@@ -23,13 +23,13 @@ resource "azurerm_windows_web_app" "tf_web_app" {
   resource_group_name = azurerm_resource_group.tf_resource_group.name
   service_plan_id     = azurerm_service_plan.tf_app_service_plan.id
 
-  
 
-  site_config { 
-    always_on = false
+
+  site_config {
+    always_on           = false
     minimum_tls_version = "1.2"
     application_stack {
-      current_stack = "dotnet"
+      current_stack  = "dotnet"
       dotnet_version = "v6.0"
     }
   }
@@ -38,9 +38,9 @@ resource "azurerm_windows_web_app" "tf_web_app" {
 
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "tf_sourcecontrol" {
-  app_id             = azurerm_windows_web_app.tf_web_app.id
-  repo_url           = "https://github.com/enes789/azure-app-service.git"
-  branch             = "main"
+  app_id                 = azurerm_windows_web_app.tf_web_app.id
+  repo_url               = "https://github.com/enes789/azure-projects.git"
+  branch                 = "inventory"
   use_manual_integration = true
-  use_mercurial      = false
+  use_mercurial          = false
 }
